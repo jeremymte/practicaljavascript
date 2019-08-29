910ca428ebd1d1d6f1d7ee436bb6c63584d0bd7a
Jeremy Te <jayte718@gmail.com>
2019-08-29 19:59:49 -0400

    modified HTML and JS file to display Todos button. Created style sheet

--- todo.js
+++ todo.js
@@ -56,3 +56,9 @@ var todoList = {
         this.displayTodos();
     }
 };
+
+var displayTodosButton = document.getElementById('displayTodosButton');
+
+displayTodosButton.addEventListener('click', function() {
+    todoList.displayTodos();
+})
