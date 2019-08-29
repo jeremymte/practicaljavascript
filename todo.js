30a45e6902016ce20b1b34595b77e624bfb4afe7
Jeremy Te <jayte718@gmail.com>
2019-08-29 17:54:14 -0400

    modified .js file to include functions within an object converting them to methods.

--- todo.js
+++ todo.js
@@ -1,24 +1,18 @@
-var todos = ['item1', 'item2', 'item3'];
-
-// It should have a function to display todos.
-function displayTodos() {
-    console.log('My todos:',todos);
-}
-
-// It should have a function to add todos.
-function addTodo(todo) {
-    todos.push(todo);
-    displayTodos();
-}
-
-// It should have a function to change todos.
-function changeTodo(position, newValue) {
-    todos[position] = newValue;
-    displayTodos();
-}
-
-// It should have a fucntion to delete todos.
-function deleteTodo(position) {
-    todos.splice(position, 1);
-    displayTodos();
-}
+var todoList = {
+    todos: ['item1', 'item2', 'item3'],
+    displayTodos: function () {
+        console.log('My todos:', this.todos)
+    },
+    addTodo: function (todo) {
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    changeTodo: function (position, newValue) {
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    deleteTodo: function (position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    }
+};
